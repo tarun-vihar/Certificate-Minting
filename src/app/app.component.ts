ded9f06190a1a5b7a2f6263c1ec184d6bd28eaaf
Tarun Vihar <tarunvihar21@gmail.com>
2023-01-20 09:20:14 -0800

    Added smart contract  to Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Web3Service} from './services/web3.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Minting';
+
+
+  constructor(private w3service:Web3Service){
+
+
+  }
+
+  login(){
+
+  
+  }
+
+  loginContract(){
+    
+    this.w3service.connectToMetaMask();
+  }
+
+  addNewUniversity(){
+
+    this.w3service.addNewUniversity()
+  }
+
+  getAllUniversities(){
+
+    this.w3service.getAllCertificates()
+  }
 }
+
+
