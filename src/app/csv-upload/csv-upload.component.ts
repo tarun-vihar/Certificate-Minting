2ce8612c797a3fe9dfc69828283c39be4f6f9b59
somasekharyepuru <somasekharyepuru@gmail.com>
2023-03-04 13:11:23 +0530

    UI::CSV Upload

--- src/app/csv-upload/csv-upload.component.ts
+++ src/app/csv-upload/csv-upload.component.ts
@@ -2,62 +2,74 @@ import { Component, OnInit } from '@angular/core';
 import { WorkBook } from '../models/WorkSheets';
 import * as XLSX from 'xlsx';
 import { AgGridModule } from 'ag-grid-angular';
+import {
+  ColDef,
+  DomLayoutType,
+  FirstDataRenderedEvent,
+} from 'ag-grid-community';
 
 @Component({
   selector: 'app-csv-upload',
   templateUrl: './csv-upload.component.html',
-  styleUrls: ['./csv-upload.component.css']
+  styleUrls: ['./csv-upload.component.css'],
 })
 export class CsvUploadComponent implements OnInit {
+  rowData: any = [];
+  selectedFile: File | null = null;
+  columnDefs: any[] = [];
+  showTable = false;
+  public domLayout: DomLayoutType = 'autoHeight';
+  public defaultColDef: ColDef = {};
 
-  rowData:any;
- 
+  constructor() {}
 
-  columnDefs = [
-    { headerName: 'ID', field: 'id' },
-    { headerName: 'Student Name', field: 'studentName' },
-    { headerName: 'Student Email', field: 'studentEmail' }
-  ];
+  ngOnInit(): void {}
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-   
-
+  onFirstDataRendered(params: FirstDataRenderedEvent) {
+    params.api.sizeColumnsToFit();
   }
-  onFileChange(event:any) {
+  async onFileChange(event: any) {
+    this.showTable = false;
     const input = event.target;
-    const reader = new FileReader();
-    reader.onload = () => {
-      const fileData = reader.result;
-      const wb = XLSX.read(fileData, { type: 'binary' });
-      const sheetName = wb.SheetNames[0];
-      const ws = wb.Sheets[sheetName];
-      console.log(sheetName)
-      this.rowData = this.toJson(ws);
-     
-    };
-    reader.readAsBinaryString(input.files[0]);
+    this.selectedFile = event?.target?.files?.[0];
+    const data = await this.selectedFile?.arrayBuffer();
+    const workbook: XLSX.WorkBook = XLSX.read(data);
+    this.prepareRowData(workbook);
   }
 
-  toJson(ws:XLSX.WorkSheet) {
-    const result : any= []
+  prepareRowData(workbook: XLSX.WorkBook) {
+    const convertedJson = this.convertWorkBookToJson(workbook);
+    if (convertedJson) {
+      this.prepareColumnDefs(convertedJson);
+    }
+    this.rowData = convertedJson;
+    this.showTable = true;
+  }
 
-    console.log(ws)
-    // workbook.SheetNames.forEach((sheetName: string ) => {
-    //   const roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    //   if (roa.length)  result.push(...roa);
+  prepareColumnDefs(json: any[]) {
+    if (json?.length) {
+      const firstRow = json[0];
+      const columnDefs: any[] = [];
+      Object.keys(firstRow).forEach((key) => {
+        columnDefs.push({
+          field: key,
+          editable: true,
+        });
+      });
+      this.columnDefs = columnDefs;
+    }
+  }
 
-    // });
-
-    const roa = XLSX.utils.sheet_to_json(ws);
-    if (roa.length)  result.push(...roa);
-    
-    
+  convertWorkBookToJson(workbook: XLSX.WorkBook) {
+    const result: any = [];
+    workbook.SheetNames.forEach((sheetName: string) => {
+      const roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+      if (roa.length) result.push(...roa);
+    });
     return result;
   }
-
-
+  saveForm() {
+    console.log(this.rowData, 'row data edited');
+    // call the api here when the data is ready
+  }
 }
